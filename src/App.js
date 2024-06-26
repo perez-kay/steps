a19e9bb9a9b2aa8c8cf2ae432c235c5e9cc0475e
Kayla Perez <perezkay@uw.edu>
2024-06-26 15:14:56 -0700

    updated state based on current state

--- src/App.js
+++ src/App.js
@@ -10,20 +10,19 @@ export default function App() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
-  }
-
-  function handleOpen() {
-    setIsOpen(!isOpen);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
     <>
-      <button className="close" onClick={handleOpen}>
+      <button
+        className="close"
+        onClick={() => setIsOpen((currView) => !currView)}
+      >
         &times;
       </button>
       {isOpen && (
